ea774a98f62bcdcd8e2b1baa26c148a98b6ccd10
Dante Chacon <107867442+dantechacon@users.noreply.github.com>
2024-06-21 20:02:43 -0300

    Update Churn.sql

--- Churn.sql
+++ Churn.sql
@@ -1,12 +1,13 @@
-/* Inicialmente, vamos modificar a formatação das datas de transações, trazendo os valores para o primeiro dia do mês e definindo-o como ref_month. 
-Isso possibilita o agrupamento dos valores por mês. */
+/* Inicialmente, vamos modificar a formatação das datas de transações, trazendo os valores para o primeiro dia do mês e definindo-o 
+como ref_month. Isso possibilita o agrupamento dos valores por mês. */
 
 WITH date_ref_monthly AS (SELECT DISTINCT DATE_FORMAT(transaction_date, '%Y-%m-01') AS ref_month
     FROM transactions), monthly_churned_customers AS (
 
-/* Nesse momento, os clientes distintos em situação de churn são contabilizados e agrupados por mês. Repare que é utilizado um left join com transactions t e drm.ref_month, 
-  dando à consulta a possibilidade de trazer um mês de referência, mesmo que não existam registros de transações. Junto a esse join, foi definido um filtro que puxa valores nulos 
-  de transações no intervalo de referência, ou seja, clientes em churn aqueles que não tiveram transações no intervalo de 28 dias. */
+/* Nesse momento, os clientes distintos em situação de churn são contabilizados e agrupados por mês. Repare que é utilizado um left join com 
+transactions t e drm.ref_month, dando à consulta a possibilidade de trazer um mês de referência, mesmo que não existam registros de 
+transações. Junto a esse join, foi definido um filtro que puxa valores nulos de transações no intervalo de referência, ou seja, clientes 
+em churn aqueles que não tiveram transações no intervalo de 28 dias. */
     
   SELECT drm.ref_month, COUNT(DISTINCT t.customer_id) AS churned_customers
     FROM date_ref_monthly drm
@@ -17,6 +18,6 @@ SELECT ref_month, churned_customers
 FROM monthly_churned_customers
 ORDER BY ref_month DESC;
 
-/* Por fim, os dados de mês referencial a clientes em churn (sem transacionar por +28 dias) são ordenados por mês e trazidos em DESC, ou seja, do mais recente para o mais antigo.
-Como dito anteriormente, em outro notebook, isso nos possibilita uma gestão mais eficiente dos dados em relatórios de visualização, pois ao definir limite de rows, é difícil haver
-limitação de dados de um mesmo mês de referência. */
+/* Por fim, os dados de mês referencial a clientes em churn (sem transacionar por +28 dias) são ordenados por mês e trazidos em DESC, 
+ou seja, do mais recente para o mais antigo. Como dito anteriormente, em outro notebook, isso nos possibilita uma gestão mais eficiente 
+dos dados em relatórios de visualização, pois ao definir limite de rows, é difícil haver limitação de dados de um mesmo mês de referência. */
