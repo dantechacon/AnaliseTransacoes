1aa17b94b9d453b08a7aec1be5e5156ddda5d3b7
Dante Chacon <107867442+dantechacon@users.noreply.github.com>
2024-06-21 20:01:55 -0300

    Update TPVRolling.sql

--- TPVRolling.sql
+++ TPVRolling.sql
@@ -1,7 +1,7 @@
 /* No primeiro momento, definimos a data de referência para ser trazida no resultado da consulta, a qual é atribuída pela data da transação, 
 de acordo com o intervalos que definirmos na subquery. Após isso, calculamos o tpv para as transações dos últimos 7 dias. 
-Para fins de documentação mais clara, o date_add surge como uma opção para tornar a janela móvel de dias mais explícita na consulta: 'INTERVAL 6 DAY' 
-que seria um intervalo de 6 dias entre uma info e outra. */
+Para fins de documentação mais clara, o date_add surge como uma opção para tornar a janela móvel de dias mais explícita na consulta: 
+'INTERVAL 6 DAY' que seria um intervalo de 6 dias entre uma info e outra. */
 
 SELECT t1.transaction_date AS ref_date, (
   SELECT SUM(t2.transaction_amount)
@@ -16,6 +16,7 @@ FROM transactions t2
 FROM transactions t1 
   ORDER BY ref_date DESC;
 
-/* É interessante notar também, que ao ordenarmos por ref_date DESC, trazemos as informações mais recentes no topo, e as mais antigas depois. No uso dessa query em ferramentas como
-looker datastudio e/ou google sheets, por exemplo, em que integraríamos a consulta via BigQuery, conseguimos trazer um limite de rows menor, deixar a visualização mais leve, e trazer
-dados mais recentes - com maior relevância numa análise do dia a dia - para o topo do dashboard.
+/* É interessante notar também, que ao ordenarmos por ref_date DESC, trazemos as informações mais recentes no topo, e as mais antigas depois.
+No uso dessa query em ferramentas como looker datastudio e/ou google sheets, por exemplo, em que integraríamos a consulta via BigQuery, 
+conseguimos trazer um limite de rows menor, deixar a visualização mais leve, e trazer dados mais recentes - com maior relevância numa 
+análise do dia a dia - para o topo do dashboard.
