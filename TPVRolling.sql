0b4352109c5d81d6e382a40cda6596f9e9620f15
Dante Chacon <107867442+dantechacon@users.noreply.github.com>
2024-06-22 13:02:56 -0300

    Update TPVRolling.sql

--- TPVRolling.sql
+++ TPVRolling.sql
@@ -1,8 +1,12 @@
-/* Inputs sobre otimização do código: Foram utilizadas ctes para calcular rolling_tpv_7 e active_customers_7 separadamente, evitando repetir 
-os cálculos para cada linha de cada table. O join utilizado foi o LEFT JOIN, para garantir que todas as datas de t1 sejam consideradas, 
-mesmo que não haja correspondência em t2, o que pode ser ajustado dependendo do objetivo (haver nulidade nos resultados ou não).*/
+/* Comentários sobre otimização do código: 
+Foram utilizadas ctes para calcular rolling_tpv_7 e active_customers_7 separadamente, evitando 
+repetir os cálculos para cada linha de cada table. O join utilizado foi o LEFT JOIN, para garantir que todas as datas de t1 sejam 
+consideradas, mesmo que não haja correspondência em t2, o que pode ser ajustado dependendo do objetivo (haver nulidade nos resultados ou não).
+Apesar de um inner join ser mais eficiente que um outer join, principalmente pra uma base de 20M de linhas, se usássemos o inner,
+os resultados excluiriam datas referenciais que não têm dados correspondentes de transações nos últimos 7 dias.*/
 
-/* No primeiro momento, definimos a data de referência para ser trazida no resultado da consulta, a qual é atribuída pela data da transação, 
+/* Detalhamento das linhas de código: 
+No primeiro momento, definimos a data de referência para ser trazida no resultado da consulta, a qual é atribuída pela data da transação, 
 de acordo com o intervalos que definirmos na subquery. Após isso, calculamos o tpv para as transações dos últimos 7 dias. 
 Para fins de documentação mais clara, o date_add surge como uma opção para tornar a janela móvel de dias mais explícita na consulta: 
 'INTERVAL 6 DAY' que seria um intervalo de 6 dias entre uma info e outra. */
@@ -29,4 +33,4 @@ ORDER BY ref_date DESC;
 /* É interessante notar também, que ao ordenarmos por ref_date DESC, trazemos as informações mais recentes no topo, e as mais antigas depois.
 No uso dessa query em ferramentas como looker datastudio e/ou google sheets, por exemplo, em que integraríamos a consulta via BigQuery, 
 conseguimos trazer um limite de rows menor, deixar a visualização mais leve, e trazer dados mais recentes - com maior relevância numa 
-análise do dia a dia - para o topo do dashboard.
+análise do dia a dia - para o topo do dashboard. Um limit de rows no código também seria uma alternativa para tornar o código mais leve. */
